ba68924d379b33068c383a5bed9273920c22122f
robert altmiller <robert.altmiller@databricks.com>
2024-09-10 13:54:20 -0500

    changes

--- code_profiler/llm_integration/llm_prompts.py
+++ code_profiler/llm_integration/llm_prompts.py
@@ -14,30 +14,19 @@ Here is the code below:\n\n
 # optimized code prompt
 code_opt_prompt = '''
 You are submitting the following Python code for an automated code review system that strictly accepts only the final optimized code version and is formatted according to Pythonic best practices.  
-Please reformat and optimize the code below to ensure it meets the system's strict criteria: focus on performance, enhance readability and maintainability, and adhere strictly to Python conventions.  
+Please reformat and optimize the code below to ensure it meets the system's strict criteria: focus on performance, enhance readability and maintainability, and adhere strictly to Python idioms and conventions.  
 Your submission should mimic a direct code commit in a professional software development environment, including appropriate code comments.  
 
-The returned answer should only be the optimized function definition like in the example below:
-def example(n: int) -> int:
-    """
-    Example comments.
-    :param n: Example parameter.
-    :return: Example return type.
-    """
-    result = 1
-    return result
+The returned answer should only be the optimized function definition like in the example below. Do not include ```Python ``` in the returned result.
 
-The returned optimized function definition answer should not look like the following:
-```python
-def example(n: int) -> int:
-    """
-    Example comments.
-    :param n: Example parameter.
-    :return: Example return type.
-    """
-    result = 1
-    return result
-```
+    def example(n: int) -> int:
+        """
+        Example comments.
+        :param n: Example parameter.
+        :return: Example return type.
+        """
+        result = 1
+        return result
 
-Here is the code below to optimize, and remember, the returned answer is only the optimized function definition with no explanations before or after:\n\n
+Here is the code below to optimize, and remember, the returned answer is only the optimized function definition and other requirements above:\n\n
 '''
