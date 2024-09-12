57aae0593c45c9f998948e0f39d63d2a8d03535a
robert altmiller <robert.altmiller@databricks.com>
2024-09-12 12:16:10 -0500

    changes

--- code_profiler/env_vars.py
+++ code_profiler/env_vars.py
@@ -79,9 +79,8 @@ print(f"log_file_write_path: {log_file_write_path}\n")
 
 # unit testing python class and functions scope (DO NOT MODIFY)
 python_class_and_fxns_scopes_unittesting = [
-    "Inventory", "is_prime", "celsius_to_fahrenheit", 
-    "factorial", "fibonacci", "reverse_string", 
-    "sum_of_list", "is_palindrome", "max_in_list", 
+    "Inventory", "is_prime", "factorial", "celsius_to_fahrenheit", 
+    "fibonacci", "reverse_string", "sum_of_list", "is_palindrome", "max_in_list", 
     "miles_to_kilometers", "count_vowels"
 ]
 print(f"python_class_and_fxns_scopes_unittesting: {python_class_and_fxns_scopes_unittesting}\n")
